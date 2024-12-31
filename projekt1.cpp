1da0d82c16c5f477a5727177c3838ca184df1d65
kpiechowicz <kpiechowicz@mat.umk.pl>
2024-12-31 22:43:04 +0100

    Dodano funkcje sprawdzajaca pierwszosc

--- projekt1.cpp
+++ projekt1.cpp
@@ -1,6 +1,17 @@
 #include <iostream>
+#include <cmath>
 
 using namespace std;
+
+bool czy_pierwsza(int n){
+    for (int i = 2; i <= sqrt(n)+1; ++i){
+        if (n % i == 0){
+            return 0;
+        }
+    }
+    return 1;
+}
+
 
 int main() {
     int a, b;
