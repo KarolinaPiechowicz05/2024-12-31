57890bb3363bc9589834d4a86edee3dd52b022aa
Karolina Piechowicz <kpiechowicz@mat.umk.pl>
2025-02-07 06:26:24 +0100

    Add files via upload

--- Projekt2KarolinaPiechowicz.c
+++ Projekt2KarolinaPiechowicz.c
@@ -15,11 +15,18 @@
 struct wiadomosc{
 	unsigned char liczba_kontrolna;
 	unsigned char nr_tury;
-	unsigned char wartosc_dodana_przez_siebie;
 	unsigned char wynik;
 	char imie[16];
 	char tekst[128];
 };
+
+signed char sprawdz_czy_cstring_jest_sama_liczba(char arr[]){
+	int i;
+	for (i = 0; i < sizeof arr && arr[i] != 0; ++i){
+		if ((arr[i] < '0' || arr[i] > '9') && arr[i] != '\n'){return 0;}
+	}
+	return 1;
+}
 
 struct addrinfo addrinfo_do_wysylania,addrinfo_do_odbierania, *wynik_wys, *wynik_odb;
 char string_ip[INET_ADDRSTRLEN];
@@ -36,10 +43,26 @@ int nr_portu_do_odbioru;
 struct sockaddr_storage niewiemjaknazwactezmienna;
 socklen_t dlugosc_adresu = sizeof niewiemjaknazwactezmienna;
 struct timeval czas;
+unsigned char ktora_faza = 0; // 0 = nadawanie, 1 = odbieranie
 unsigned char nr_gracza = 0;
 unsigned char nr_ostatniej_tury = 0;
 unsigned char wynik_gry = 0;
 unsigned char kto_zwyciezca;
+
+//liczba kontrolna = 6
+void koniec(){
+	if (wiadomosc_przychodzaca.liczba_kontrolna != 6){
+		printf("Konczymy gre.\n");
+		wiadomosc_wychodzaca.liczba_kontrolna = 6;
+		liczba_bajtow_przeslanych = sendto(gniazdo_wys,&wiadomosc_wychodzaca,sizeof wiadomosc_wychodzaca,0,wynik_wys->ai_addr,wynik_wys->ai_addrlen);
+		if (liczba_bajtow_przeslanych == -1){perror("Blad sendto (koniec)");exit(1);}
+	}
+	else{printf("%s zakonczyl gre.\n",wiadomosc_przychodzaca.imie);}
+	close(gniazdo_odb);
+	freeaddrinfo(wynik_wys);
+	freeaddrinfo(wynik_odb);
+	exit(1);
+}
 
 //nie chce mi sie myslec wiec po prostu podkradne funkcje get_in_addr z listener'a beej'a. wlasciwie to i tak to tu jest zbedne bo nie wspiera ipv6 ale no
 // get sockaddr, IPv4 or IPv6:
@@ -52,17 +75,19 @@ void *get_in_addr(struct sockaddr *sa){
 }
 
 void wypisz_wiadomosc(struct wiadomosc wiad){
-	printf("[%s]: %s",wiad.imie,wiad.tekst);
+	printf("%s napisal: %s",wiad.imie,wiad.tekst);
 }
 
 //do chatu. liczba kontrolna = 5
 void wyslij_wiadomosc(){
-	printf("[GRA] Podaj wiadomosc do wyslania (jesli wiadomosc jest pusta, nie bedzie wyslana):\n> ");
+	//printf("[GRA] Podaj wiadomosc do wyslania (jesli wiadomosc jest pusta, nie bedzie wyslana):\n> ");
+	//printf("> ");
 	//kochac getline'a kochac (tekst ocenzurowany)
 	//char *temp_tekst;
 	//size_t rozmiar = (sizeof wiadomosc_wychodzaca.tekst)-1;
 	//getline(&temp_tekst,&rozmiar,stdin);
-	if (fgets(wiadomosc_wychodzaca.tekst, sizeof(wiadomosc_wychodzaca.tekst), stdin) == NULL){printf("[GRA] Blad fgets\n");exit(1);}
+	//printf("Podaj wiadomosc tekstowa\n");
+	if (fgets(wiadomosc_wychodzaca.tekst, sizeof(wiadomosc_wychodzaca.tekst), stdin) == NULL){printf("Blad fgets\n");exit(1);}
 	wiadomosc_wychodzaca.liczba_kontrolna = 5;
 	//printf("[GRA] Wiadomosc pobrana.\n");
 	//snprintf(wiadomosc_wychodzaca.tekst, 254, "%s", temp_tekst);
@@ -70,8 +95,10 @@ void wyslij_wiadomosc(){
 	//printf("[GRA] Wiadomosc skopiowana do struktury: %s",wiadomosc_wychodzaca.tekst);
 	if (strcmp(wiadomosc_wychodzaca.tekst,"") == 0 || strcmp(wiadomosc_wychodzaca.tekst,"\n") == 0){}
 	else{
+		if (sprawdz_czy_cstring_jest_sama_liczba(wiadomosc_wychodzaca.tekst) == 1 && ktora_faza == 1){printf("Teraz tura gracza %s, poczekaj na swoja kolej.\n",wiadomosc_przychodzaca.imie);} // specjalna funkcja tylko po to by czat byl gorszy nice
+		if (strcmp(wiadomosc_wychodzaca.tekst,"koniec") == 0 || strcmp(wiadomosc_wychodzaca.tekst,"koniec\n") == 0){koniec();}
 		liczba_bajtow_przeslanych = sendto(gniazdo_wys,&wiadomosc_wychodzaca,sizeof wiadomosc_wychodzaca,0,wynik_wys->ai_addr,wynik_wys->ai_addrlen);
-		if (liczba_bajtow_przeslanych == -1){perror("[GRA] Blad sendto (chat)");exit(1);}
+		if (liczba_bajtow_przeslanych == -1){perror("Blad sendto (chat)");exit(1);}
 		//printf("[GRA] Wiadomosc wyslana.\n");
 		}
 }
@@ -93,6 +120,9 @@ void odbierz_wiadomosc(){
 		//wartosc podana przez gracza w grze
 		else if (wiadomosc_przychodzaca.liczba_kontrolna == 3){
 			wynik_gry = wiadomosc_przychodzaca.wynik;
+		}
+		else if (wiadomosc_przychodzaca.liczba_kontrolna == 6){
+			koniec();
 		}
 	}
 }
@@ -120,6 +150,9 @@ void odbierz_wiadomosc_z_timeoutem(){
 			else if (wiadomosc_przychodzaca.liczba_kontrolna == 3){
 			wynik_gry = wiadomosc_przychodzaca.wynik;
 			}
+			else if (wiadomosc_przychodzaca.liczba_kontrolna == 6){
+				koniec();
+			}
 		}
 	}
 	
@@ -131,58 +164,56 @@ void odbierz_wiadomosc_z_timeoutem(){
 void powiedz_drugiemu_graczowi_ze_jest_drugim_graczem(){
 	wiadomosc_wychodzaca.liczba_kontrolna = 4;
 	wiadomosc_wychodzaca.nr_tury = 0;
-	wiadomosc_wychodzaca.wartosc_dodana_przez_siebie = 0;
 	wiadomosc_wychodzaca.wynik = 0;
 	strcpy (wiadomosc_wychodzaca.tekst,"");
 	liczba_bajtow_przeslanych = sendto(gniazdo_wys,&wiadomosc_wychodzaca,sizeof wiadomosc_wychodzaca,0,wynik_wys->ai_addr,wynik_wys->ai_addrlen);
 	if (liczba_bajtow_przeslanych == -1){perror("[GRA] Blad sendto (inicjalizacja gracz 1)");exit(1);}
-	printf("[GRA] Przeslano drugiemu graczowi znak\n");
+	//printf("[GRA] Przeslano drugiemu graczowi znak\n");
 }
 
 //liczba kontrolna = 2
 void powiedz_pierwszemu_graczowi_ze_jestem_drugim_graczem(){
 	wiadomosc_wychodzaca.liczba_kontrolna = 2;
 	wiadomosc_wychodzaca.nr_tury = 0;
-	wiadomosc_wychodzaca.wartosc_dodana_przez_siebie = 0;
 	wiadomosc_wychodzaca.wynik = 0;
 	strcpy (wiadomosc_wychodzaca.tekst,"");
 	liczba_bajtow_przeslanych = sendto(gniazdo_wys,&wiadomosc_wychodzaca,sizeof wiadomosc_wychodzaca,0,wynik_wys->ai_addr,wynik_wys->ai_addrlen);
 	if (liczba_bajtow_przeslanych == -1){perror("[GRA] Blad sendto (inicjalizacja gracz 2)");exit(1);}
-	printf("[GRA] Przeslano pierwszemu graczowi znak spowrotem\n");
+	//printf("[GRA] Przeslano pierwszemu graczowi znak spowrotem\n");
 }
 
 void wyznacz_pierwszego_gracza(){
 	//ustala timeout recv na 3 sekund
 	czas.tv_sec = 3; czas.tv_usec = 0;
-	if (setsockopt(gniazdo_odb, SOL_SOCKET, SO_RCVTIMEO, &czas, sizeof(czas)) < 0) {perror("[GRA] setsockopt");exit(1);}
+	if (setsockopt(gniazdo_odb, SOL_SOCKET, SO_RCVTIMEO, &czas, sizeof(czas)) < 0) {perror("Blad setsockopt");exit(1);}
 	
 	liczba_bajtow_odebranych = recvfrom(gniazdo_odb,&wiadomosc_przychodzaca,sizeof wiadomosc_przychodzaca,0,(struct sockaddr *)&niewiemjaknazwactezmienna,&dlugosc_adresu);
 	
 	if (liczba_bajtow_odebranych == -1 && (errno == EAGAIN)){
-		printf("[GRA] Nie znaleziono drugiego gracza. Jestes graczem nr 1. Jesli tak nie powinno byc, sprobuj ponownie.\n");
+		//printf("[GRA] Nie znaleziono drugiego gracza. Jestes graczem nr 1. Jesli tak nie powinno byc, sprobuj ponownie.\n");
 		powiedz_drugiemu_graczowi_ze_jest_drugim_graczem();
 		nr_gracza = 0;
-		printf("[GRA] Czekam na odpowiedz...\n");
+		//printf("[GRA] Czekam na odpowiedz...\n");
 		
 		liczba_bajtow_odebranych = recvfrom(gniazdo_odb,&wiadomosc_przychodzaca,sizeof wiadomosc_przychodzaca,0,(struct sockaddr *)&niewiemjaknazwactezmienna,&dlugosc_adresu);
-		if (liczba_bajtow_odebranych == -1 && (errno == EAGAIN)){printf("[GRA] Nie znaleziono drugiego gracza. Sprobuj ponownie.\n");exit(1);}
-		else if (liczba_bajtow_odebranych == -1){perror("[GRA] Blad recvfrom"); exit(1);}
-		printf("[GRA] Odpowiedz odebrano! Mozna zaczynac rozgrywke.\n");
+		if (liczba_bajtow_odebranych == -1 && (errno == EAGAIN)){printf("Nie znaleziono drugiego gracza. Sprobuj ponownie.\n");exit(1);}
+		else if (liczba_bajtow_odebranych == -1){perror("Blad recvfrom"); exit(1);}
+		//printf("[GRA] Odpowiedz odebrano! Mozna zaczynac rozgrywke.\n");
 		
 	}
 	else if (liczba_bajtow_odebranych == -1){perror("[GRA] Blad recvfrom"); exit(1);}
 	else{
-		printf("[GRA] Odebrano znak... ");
+		//printf("[GRA] Odebrano znak... ");
 		char s[INET_ADDRSTRLEN];
-		printf("Oczekiwane IP: %s, otrzymane IP: %s",string_ip, inet_ntop(niewiemjaknazwactezmienna.ss_family, get_in_addr((struct sockaddr *)&niewiemjaknazwactezmienna), s, sizeof s));
+		//printf("Oczekiwane IP: %s, otrzymane IP: %s",string_ip, inet_ntop(niewiemjaknazwactezmienna.ss_family, get_in_addr((struct sockaddr *)&niewiemjaknazwactezmienna), s, sizeof s));
 		if (strcmp(string_ip,inet_ntop(niewiemjaknazwactezmienna.ss_family,
 			get_in_addr((struct sockaddr *)&niewiemjaknazwactezmienna),
-			s, sizeof s)) != 0){printf(" ...Niezgodne! Zamykam.\n");exit(1);}
-		printf(" ...Zgodne!\n");
-		printf("[GRA] Sprawdzam zgodnosc znaku...");
-		if (wiadomosc_przychodzaca.liczba_kontrolna != 4 || wiadomosc_przychodzaca.nr_tury != 0 || wiadomosc_przychodzaca.wartosc_dodana_przez_siebie != 0 || wiadomosc_przychodzaca.wynik != 0){printf(" ...Niezgodny! Zamykam.\n");exit(1);}
-		printf(" ...Zgodny!\n");
-		printf("[GRA] Odnaleziono pierwszego gracza. Jestes graczem nr 2.\n");
+			s, sizeof s)) != 0){printf(" ...Niezgodny adres IP! Zamykam.\n");exit(1);}
+		//printf(" ...Zgodne!\n");
+		//printf("[GRA] Sprawdzam zgodnosc znaku...");
+		if (wiadomosc_przychodzaca.liczba_kontrolna != 4 || wiadomosc_przychodzaca.nr_tury != 0  || wiadomosc_przychodzaca.wynik != 0){printf(" ...Niezgodny znak! Zamykam.\n");exit(1);}
+		//printf(" ...Zgodny!\n");
+		//printf("[GRA] Odnaleziono pierwszego gracza. Jestes graczem nr 2.\n");
 		nr_gracza = 1;
 		powiedz_pierwszemu_graczowi_ze_jestem_drugim_graczem();
 	}
@@ -191,47 +222,62 @@ void wyznacz_pierwszego_gracza(){
 }
 
 void wylosuj_liczbe_poczatkowa(){
-	printf("[GRA] Losuje poczatkowa liczbe...");
 	wiadomosc_wychodzaca.wynik = (rand() % 10) + 1;
 	wynik_gry = wiadomosc_wychodzaca.wynik;
-	printf(" ...Wylosowano! To %d ;)\n",wiadomosc_wychodzaca.wynik);
+	printf("Losowa wartosc poczatkowa: %d, podaj kolejna wartosc\n",wiadomosc_wychodzaca.wynik);
 }
 
 void faza_odbierania(){
+	ktora_faza = 1;
 	wiadomosc_przychodzaca.liczba_kontrolna = 0;
-	printf("[GRA] Oczekuje na odpowiedz od wspolgracza. W miedzyczasie mozesz do niego wysylac wiadomosci.\n");
+	unsigned char czy_bylo_powtorzenie = 0;
+	//printf("[GRA] Oczekuje na odpowiedz od wspolgracza. W miedzyczasie mozesz do niego wysylac wiadomosci.\n");
 	while (wiadomosc_przychodzaca.liczba_kontrolna != 3){
 		odbierz_wiadomosc_z_timeoutem();
+		if (czy_bylo_powtorzenie != 0 || (nr_gracza == 1 && nr_ostatniej_tury == 1)) printf("> ");
 		wyslij_wiadomosc();
+		czy_bylo_powtorzenie = 1;
 	}
 }
 
 //liczba kontrolna = 3 (dla wysylania liczby)
 void faza_nadawania(){
+	ktora_faza = 0;
 	//printf("[GRA] Obecny numer tury: %d\n",nr_ostatniej_tury);
 	int liczba = 0; 
+	unsigned char czy_bylo_powtorzenie = 0;
 	while (liczba == 0){
 		//printf("[GRA] Weszla w while, liczba=%d\n",liczba);
-		printf("[GRA] Podaj liczbe od 1-10 by przeslac wspolgraczowi, albo 11 by wyslac wiadomosc przez chat.\n> ");
-		scanf("%d",&liczba);
-		getchar();
-		if (liczba == 11)   {wyslij_wiadomosc();   liczba = 0;   }
-		else if(liczba >= 1 && liczba <= 10){
+		if ( (!(nr_gracza == 0 && nr_ostatniej_tury == 0)) && czy_bylo_powtorzenie == 0) printf("%s podal wartosc %d, podaj kolejna wartosc.\n",wiadomosc_przychodzaca.imie,wiadomosc_przychodzaca.wynik);
+		//printf("Podaj liczbe lub koniec lub czat");
+		printf("> ");
+		char liczba_char[16];
+		scanf("%s",liczba_char);
+		liczba = atoi(liczba_char);
+		//getchar();
+		if (strcmp(liczba_char,"koniec") == 0 || strcmp(liczba_char,"koniec\n") == 0){czy_bylo_powtorzenie = 1; koniec();}
+		else if (strcmp(liczba_char,"czat") == 0 || strcmp(liczba_char,"czat\n") == 0 || strcmp(liczba_char,"chat") == 0 || strcmp(liczba_char,"chat\n") == 0)   {
+			char zlap_newline;
+			scanf("%c",&zlap_newline);
+			printf("> ");
+			wyslij_wiadomosc();
+			liczba = 0;
+			czy_bylo_powtorzenie = 1;
+			}
+		else if(liczba - wynik_gry >= 1 && liczba - wynik_gry <= 10){
 			wiadomosc_wychodzaca.liczba_kontrolna = 3;
-			wynik_gry += liczba;
+			wynik_gry = liczba;
 			wiadomosc_wychodzaca.wynik = wynik_gry;
-			wiadomosc_wychodzaca.wartosc_dodana_przez_siebie = liczba;
 			nr_ostatniej_tury += 1;
 			wiadomosc_wychodzaca.nr_tury = nr_ostatniej_tury;
-			printf("[GRA] Wynik gry: %d\n",wynik_gry);
+			//printf("[GRA] Wynik gry: %d\n",wynik_gry);
 			liczba_bajtow_przeslanych = sendto(gniazdo_wys,&wiadomosc_wychodzaca,sizeof wiadomosc_wychodzaca,0,wynik_wys->ai_addr,wynik_wys->ai_addrlen);
-			if (liczba_bajtow_przeslanych == -1){perror("[GRA] Blad sendto (nadawanie)");exit(1);}
+			if (liczba_bajtow_przeslanych == -1){perror("Blad sendto (nadawanie)");exit(1);}
 		}
-		else {printf("[GRA] Nieprawidlowa liczba: %d!\n",liczba); liczba = 0;}
+		else  {printf("Takiej wartosci nie mozesz wybrac!\n"); liczba = 0; czy_bylo_powtorzenie = 1; }
 		while (liczba_bajtow_odebranych != -1){odbierz_wiadomosc_z_timeoutem();}
 		liczba_bajtow_odebranych = 0;
 	}
-	
 }
 
 /*-----------------------------------------------------------------------------------------------------------------------------------------------------*/
@@ -249,10 +295,10 @@ void faza_nadawania(){
 
 int main(int argc, char **argv){
 	srand(time(NULL));
-	if (argc != 3 && argc != 4){printf("[GRA] Nieprawidlowa ilosc argumentow: %d. Oczekiwano 3 lub 4.\n",argc);exit(1);}
-	if (argc == 4 && strlen(argv[3]) > 15){printf("[GRA] Nieprawidlowa dlugosc nicku: %zu. Oczekiwano nie wieksza niz 15.\n",strlen(argv[3]));exit(1);}
+	if (argc != 3 && argc != 4){printf("Nieprawidlowa ilosc argumentow: %d. Oczekiwano 3 lub 4.\n",argc);exit(1);}
+	if (argc == 4 && strlen(argv[3]) > 15){printf("Nieprawidlowa dlugosc nicku: %zu. Oczekiwano nie wieksza niz 15.\n",strlen(argv[3]));exit(1);}
 	nr_portu_do_wysylki = atoi(argv[2]);
-	if (nr_portu_do_wysylki <= 1024){printf("[GRA] Podany nr portu %d jest za niski, oczekiwano wiekszych od 1024.\n",nr_portu_do_wysylki);exit(1);}
+	if (nr_portu_do_wysylki <= 1024){printf("Podany nr portu %d jest za niski, oczekiwano wiekszych od 1024.\n",nr_portu_do_wysylki);exit(1);}
 	
 	memset(&addrinfo_do_wysylania, 0, sizeof addrinfo_do_wysylania);
 	memset(&addrinfo_do_odbierania, 0, sizeof addrinfo_do_odbierania);
@@ -266,20 +312,22 @@ int main(int argc, char **argv){
 		
 	//wyznacza IP jesli argv[1] to adres domenowy. znaczy, funkcja dziala i gdy [1] to ip, ale nie ma rozroznienia, i tak wyznacza ip
 	
-	if ((status = getaddrinfo(argv[1], argv[2], &addrinfo_do_wysylania, &wynik_wys)) != 0) {fprintf(stderr, "[GRA] Blad getaddrinfo: %s\n", gai_strerror(status));exit(1);}
-	if ((status = getaddrinfo(NULL, argv[2], &addrinfo_do_odbierania, &wynik_odb)) != 0) {fprintf(stderr, "[GRA] Blad getaddrinfo: %s\n", gai_strerror(status));exit(1);}
+	if ((status = getaddrinfo(argv[1], argv[2], &addrinfo_do_wysylania, &wynik_wys)) != 0) {fprintf(stderr, "Blad getaddrinfo: %s\n", gai_strerror(status));exit(1);}
+	if ((status = getaddrinfo(NULL, argv[2], &addrinfo_do_odbierania, &wynik_odb)) != 0) {fprintf(stderr, "Blad getaddrinfo: %s\n", gai_strerror(status));exit(1);}
 	
 	//wydziela ip z wyznaczonego 
 	addr_drugiego_gracza = (struct sockaddr_in *)wynik_wys->ai_addr;
-	if (inet_ntop(AF_INET,&(addr_drugiego_gracza->sin_addr),string_ip,INET_ADDRSTRLEN) == NULL){perror("[GRA] Blad inet_ntop");exit(1);}
+	if (inet_ntop(AF_INET,&(addr_drugiego_gracza->sin_addr),string_ip,INET_ADDRSTRLEN) == NULL){perror("Blad inet_ntop");exit(1);}
+	
 	//ustala nick uzytkownika uzywany w czacie
 	if (argc == 3){strcpy(wiadomosc_wychodzaca.imie,string_ip);}
 	else{strcpy(wiadomosc_wychodzaca.imie,argv[3]);}
-	//printf("[GRA] IP: %s\n", string_ip);
+	
+	
 	gniazdo_odb = socket(wynik_odb->ai_family, wynik_odb->ai_socktype,wynik_odb->ai_protocol);
 	gniazdo_wys = socket(wynik_wys->ai_family, wynik_wys->ai_socktype,wynik_wys->ai_protocol);
-	if (gniazdo_odb == -1 || gniazdo_wys == -1){perror("[GRA] Blad socket");exit(1);}
-	//patrzac na beej'a, bind() jest tylko w listenerze, nie w talkerze? huh
+	
+	if (gniazdo_odb == -1 || gniazdo_wys == -1){perror("Blad socket");exit(1);}
 	if (bind(gniazdo_odb,wynik_odb->ai_addr,wynik_odb->ai_addrlen) == -1){perror("[GRA] Blad bind");exit(1);}
 		
 	/*
@@ -291,7 +339,8 @@ int main(int argc, char **argv){
 	
 	Seems a bit strange, but I don't see why not. connect() just sets the default destination address/port for the socket. (Have you tried it? If it doesn't work for some reason, just use sendto().) Personally I'd just use sendto() because otherwise you'll get confused if multiple clients connect to your server. – mpontillo
 	*/
-	printf("[GRA] Witamy w grze! Oczekujemy na drugiego gracza przez 3 sekundy...\n");
+	printf("Gra w 50, wersja A.\n");
+	printf("Rozpoczynam gre z %s. Napisz \"koniec\" aby zakonczyc.\n",string_ip);
 	wyznacz_pierwszego_gracza();
 	
 	if (nr_gracza == 0){
@@ -312,13 +361,9 @@ int main(int argc, char **argv){
 			if (wynik_gry >= 50){kto_zwyciezca = 3; break;}
 		}
 	}
-	printf("[GRA] Liczba wyniosla 50. Wygral");
-	if (kto_zwyciezca % 2 == 1){printf("es Ty!\n");}
-	else{printf(" Twoj wspolgracz!\n");}
-	printf("[GRA] Zakonczono gre.\n");
-	
-	
-	
+	if (kto_zwyciezca % 2 == 1){printf("Wygrana!\n");}
+	else{printf("Przegrana!\n");}
+		
 	
 	close(gniazdo_odb);
 	
